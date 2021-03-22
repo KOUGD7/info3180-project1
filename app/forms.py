6f2b0de9d872973859c0f0edc7581dc62ccdbc10
KOUGD7 <cougahdere@yahoo.com>
2021-03-21 23:03:22 -0500

    Update Property Page Layout

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField
 from wtforms import SelectField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, Length, NumberRange
 from wtforms.widgets import TextArea
 
 
@@ -17,7 +17,7 @@ class UploadForm (FlaskForm):
     my_choices = [('House', 'House'), ('Apartment', 'Apartment')]
     types = SelectField(choices = my_choices, default = ['House'])
 
-    description = StringField('Description', widget=TextArea())
+    description = StringField('Description', widget=TextArea(), validators=[Length(min=- 1, max=255, message=None)])
 
     upload = FileField('Photo', validators=[
         FileRequired(),
