711c71e1590113ff00d60347066a34c3a3f29046
KOUGD7 <cougahdere@yahoo.com>
2021-03-23 20:10:23 -0500

    Update Upload Fields, Error Message Display and Page Layout

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField
-from wtforms import SelectField
+from wtforms import StringField, SelectField, IntegerField, DecimalField
 from wtforms.validators import DataRequired, Email, Length, NumberRange
 from wtforms.widgets import TextArea
 
@@ -9,15 +8,15 @@ from wtforms.widgets import TextArea
 class UploadForm (FlaskForm):
 
     title = StringField('Property Title', validators=[DataRequired()])
-    bedroom = StringField('No. Of Bedrooms', validators=[DataRequired()])
-    bathroom = StringField('No. Of Bathrooms', validators=[DataRequired()])
+    bedroom = IntegerField('No. Of Bedrooms', validators=[DataRequired()])
+    bathroom = IntegerField('No. Of Bathrooms', validators=[DataRequired()])
     location = StringField('Location', validators=[DataRequired()])
-    price = StringField('Price', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()], places=2)
 
     my_choices = [('House', 'House'), ('Apartment', 'Apartment')]
     types = SelectField(choices = my_choices, default = ['House'])
 
-    description = StringField('Description', widget=TextArea(), validators=[Length(min=- 1, max=255, message=None)])
+    description = StringField('Description', widget=TextArea(), validators=[Length(max=255, message = "Max lenght is 255")])
 
     upload = FileField('Photo', validators=[
         FileRequired(),
